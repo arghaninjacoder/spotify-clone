09c3514ff4cb452533564968c0a43569a3c167db
Argha Das <arghadas@Arghas-MacBook-Air.local>
2023-06-22 09:38:41 +0530

    Search feature implemented with debounce

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,10 +1,37 @@
 "use client";
 
-import type { FC } from "react";
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useState, type FC, useEffect } from "react";
+import qs from "query-string";
+import Input from "./Input";
 
 interface SearchInputProps {}
 
 const SearchInput: FC<SearchInputProps> = ({}) => {
-  return <div>Search Input</div>;
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce(value, 500);
+
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
+
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query,
+    });
+
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <Input
+      placeholder="What do you want to listen to ?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 };
 export default SearchInput;
